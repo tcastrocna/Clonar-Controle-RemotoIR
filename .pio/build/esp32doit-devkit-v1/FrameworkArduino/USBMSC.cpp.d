47522cd79ab3079f90d4be553c417b4e5c3edf34
Tiago Castro <tiagocastrodacunha3005@gmail.com>
2022-06-20 16:49:23 -0300

    Alteração no Projeto
    Troca de estrutura, adequada para a plataforma arduino IDE.

--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.o: \
- C:/Users/tiago/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- C:/Users/tiago/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/tiago/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/config/sdkconfig.h
--- .pio/build/esp32doit-devkit-v1/lib325/ESP32-IRremote/irPronto.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32doit-devkit-v1/lib325/ESP32-IRremote/irPronto.cpp.o: \
- .pio/libdeps/esp32doit-devkit-v1/ESP32-IRremote/irPronto.cpp
